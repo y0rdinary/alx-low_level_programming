e9cefe8c7e1901e814293a79908d1559e39099a0
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-06-22 04:06:40 -0700

    print message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * printf: prints a string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char My_message[] = "with proper grammar, but the outcome is a piece of art,";
+
+printf("%s\n", My_message);
+return (0);
+}
