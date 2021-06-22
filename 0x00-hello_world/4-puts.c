2ed9df56d310299f149dbbf56c2fd9c59a9e607e
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-06-22 03:58:42 -0700

    put message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * puts: print string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char My_msg[] = "\"Programming is like building a multilingual puzzle";
+
+  puts(My_msg);
+  return (0);
+}
