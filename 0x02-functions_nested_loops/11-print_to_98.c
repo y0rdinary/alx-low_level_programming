a43f16a995dbe0445d92896f1b976dd89f9a04d2
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-06-22 11:15:11 -0700

    to 98 message

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* print_to_98 - prints integer numbers from n to 98.
+* @n: n is an integer number.
+* Return: void.
+*/
+void print_to_98(int n)
+{
+if (n >= 98)
+{
+while (n > 98)
+printf("%d, ", n--);
+printf("%d\n", n);
+}
+else
+{
+while (n < 98)
+printf("%d, ", n++);
+printf("%d\n", n);
+}
+}
