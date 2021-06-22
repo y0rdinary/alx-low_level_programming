9e504d54dd1b2d435e2d7f2d967f122d3d1dc081
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-06-22 04:10:09 -0700

    size commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * sizeof: return memory allocated to a data type
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %u byte(s)\n", sizeof(char));
+printf("Size of an int: %u byte(s)\n", sizeof(int));
+printf("Size of a long int: %u byte(s)\n", sizeof(long));
+printf("Size of a long long int: %u byte(s)\n", sizeof(long long));
+printf("Size of a float: %u byte(s)\n", sizeof(float));
+return (0);
+}
