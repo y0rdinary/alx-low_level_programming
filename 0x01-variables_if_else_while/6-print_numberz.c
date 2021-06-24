3af1d0c1b15008a5aab9a873512b0db82e9d7805
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-06-24 05:48:30 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - a function to print the 10 digit of common numbers
+*
+* Return: always returns zero
+*/
+
+int main(void)
+{
+int a = '0';
+
+while (a <= '9')
+{
+putchar(a);
+a++;
+}
+
+putchar('\n');
+
+return (0);
+}
