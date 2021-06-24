f7b896a07111592559562e4177518262937ff686
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-06-24 05:46:03 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a = 0;
+do {
+printf("%d", a);
+a++;
+} while (a < 10);
+printf("\n");
+return (0);
+}
