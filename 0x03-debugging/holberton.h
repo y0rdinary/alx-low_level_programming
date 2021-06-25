58c3baf83496cc4278787d7d17eba7745b4c2724
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-06-24 22:44:17 -0700

    debugging exercise

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+int largest_number(int a, int b, int c);
+
+#endif /* HOLBERTON_H */
