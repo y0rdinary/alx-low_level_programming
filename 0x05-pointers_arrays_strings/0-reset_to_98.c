824669af9007eb8c5f81b2bb491d7f1f0a6635c6
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-06-28 05:50:40 -0700

    pointers arrays strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 
 /**
- *reset_to_98 - it reset numbers to 98
- *@n: an integer value,
- *Return: returns nothing in void cases
- */
+*reset_to_98 - it reset numbers to 98
+*@n: an integer value,
+*Return: returns nothing in void cases
+*/
 
 void reset_to_98(int *n)
 {
-  *n = 98;
+*n = 98;
 }
