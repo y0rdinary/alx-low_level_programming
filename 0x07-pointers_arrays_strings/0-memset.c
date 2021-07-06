53690ff43ba23975a5f2e5708a321ebbff5be3cc
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-07-06 05:54:50 -0700

    more pointers arrays strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,12 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i = 0	;
+unsigned int i = 0;
 
 while (i < n)
 {
-s[i] = b	;
-i++	;
+s[i] = b;
+i++;
 }
-return (s)		;
+return (s);
 }
