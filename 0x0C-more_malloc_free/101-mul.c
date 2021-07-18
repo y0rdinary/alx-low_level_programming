9161f297f3de33870e6086e73c9997600f440d3c
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-07-18 06:21:49 -0700

    more malloc free

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *mul - a function to multiply arguments
+ *@argv: accepts argument and builds an array
+ *@argc: counts the arguments
+ *Return: returns
+ */
+
+int mul(int argc, unsigned int *argv)
+{
+if (argc > 2)
+printf("Error\n");
+exit(98);
+
+if (argv[0]/10 != 0 || argv[1]/10 != 0)
+printf("Error\n");
+exit(98);
+
+int ans = argv[0] * argv[1];
+printf(ans);
+
+return (0);
+}
