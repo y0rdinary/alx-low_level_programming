582d9b00233c405444040a09da5a79ebf329f36f
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-07-18 05:48:07 -0700

    malloc free

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,74 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * number - function to count number of words
+ * @str: string being passed to check for words
+ *
+ * Return: number of words
+ */
+int number(char *str)
+{
+int a, num = 0;
+
+for (a = 0; str[a] != '\0'; a++)
+{
+if (*str == ' ')
+str++;
+else
+{
+for (; str[a] != ' ' && str[a] != '\0'; a++)
+str++;
+num++;
+}
+}
+return (num);
+}
+
+/**
+ * strtow - function that splits string into words
+ * @str: string being passed
+ * Return: null if string is empty or null or function fails
+ */
+char **strtow(char *str)
+{
+int total_words = 0, b = 0, c = 0, length = 0;
+char **words, *found_word;
+
+if (str == 0 || *str == 0)
+return (NULL);
+total_words = number(str);
+if (total_words == 0)
+return (NULL);
+words = malloc((total_words + 1) * sizeof(char *));
+if (words == 0)
+return (NULL);
+for (; *str != '\0' &&  b < total_words;)
+{
+if (*str == ' ')
+str++;
+else
+{
+found_word = str;
+for (; *str != ' ' && *str != '\0';)
+{
+length++;
+str++;
+}
+words[b] = malloc((length + 1) * sizeof(char));
+while (*found_word != ' ' && *found_word != '\0')
+{
+words[b][c] = *found_word;
+found_word++;
+c++;
+}
+words[b][c] = '\0';
+b++;
+c = 0;
+length = 0;
+str++;
+}
+}
+return (words);
+}
