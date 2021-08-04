f797e2474526dedbc84261144402b60998bb27f0
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-08-04 04:13:49 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - a function to print the fil name of its source file
+ *Return: returns 0.
+ */
+
+int main(void)
+{
+  printf("%s\n", __FILE__);
+  return (0);
+}
