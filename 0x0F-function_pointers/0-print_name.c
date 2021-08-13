831561deedf98f36ebc3b7219d70f3431caa045a
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-08-12 23:56:58 -0700

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - a function to print a name
+ *
+ *@name: name variable pointer
+ *@f: a function pointer
+ *Return: returns nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+  if (name && f)
+    {
+      f(name);
+    }
+}
