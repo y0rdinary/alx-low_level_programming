ab8585fe9e57b8479d1c06fb3fc183a9512ccf7f
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2021-10-04 01:49:42 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,28 +10,28 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-  unsigned int result, base;
-  int i;
+unsigned int result, base;
+int i;
 
-  result = 0;
-  i = 0;
-  base = 1;
+result = 0;
+i = 0;
+base = 1;
 
-  if (!b)
-    return (0);
+if (!b)
+return (0);
 
-  while (*(b + i))
-    {
-      if (*(b + i) != '0' && *(b + i) != '1')
-	return (0);
-      i++;
-    }
-  for (i--; i >= 0; i--)
-    {
-      if (*(b + i) == '1')
-	result = result + base;
-      base = base * 2;
-    }
+while (*(b + i))
+{
+if (*(b + i) != '0' && *(b + i) != '1')
+return (0);
+i++;
+}
+for (i--; i >= 0; i--)
+{
+if (*(b + i) == '1')
+result = result + base;
+base = base * 2;
+}
 
-  return (result);
+return (result);
 }
